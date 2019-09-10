c0e367150cbd78ba3339c8d2c994d88fc9bd8bb4
Ollie Crook <ollie@hubblehq.com>
2019-09-10 18:34:53 +0100

    initlal styling for ScoreBoard

--- components/comps/ScoreBoard.js
+++ components/comps/ScoreBoard.js
@@ -2,12 +2,41 @@ import { css } from 'emotion';
 import PropTypes from 'prop-types';
 import React from 'react';
 
+const scoreBoardStyle = css`
+  font-family: 'Courier New', Courier, monospace;
+  margin-bottom: 20px;
+`;
+
+const scoresStyle = css`
+  display: flex;
+  justify-content: center;
+`;
+
+const scoreStyle = css`
+  align-items: center;
+  border-top: 2px solid black;
+  border-right: 1px solid black;
+  border-bottom: 2px solid black;
+  border-left: 2px solid black;
+  display: flex;
+  height: 30px;
+  justify-content: center;
+  width: 30px;
+
+  &:last-child {
+    border-right: 2px solid black;
+  }
+`;
+
 const ScoreBoard = ({ scores }) => (
-  <>
-    {scores.map(score => (
-      <p>{score}</p>
-    ))}
-  </>
+  <div className={scoreBoardStyle}>
+    <h2>HIGH SCORE</h2>
+    <div className={scoresStyle}>
+      {scores.map(score => (
+        <span className={scoreStyle}>{score}</span>
+      ))}
+    </div>
+  </div>
 );
 
 export default ScoreBoard;
