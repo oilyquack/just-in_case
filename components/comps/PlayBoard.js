36b87054e4318f5750390ffee4d21b5af1d57776
Ollie Crook <ollie@hubblehq.com>
2019-09-13 15:05:37 +0100

    Add tremble animation to questions in PlayBoard because I am a sadist

--- components/comps/PlayBoard.js
+++ components/comps/PlayBoard.js
@@ -1,10 +1,47 @@
-import { css } from 'emotion';
+import { css, keyframes } from 'emotion';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 import theme from '../../theme';
 
+const trembleAnimation = keyframes`
+  0% {
+    transform: translate(0.1px, 0.1px) rotate(0deg);
+  }
+  10% {
+    transform: translate(-0.1px, -0.2px) rotate(-1deg);
+  }
+  20% {
+    transform: translate(-0.3px, 0.0px) rotate(1deg);
+  }
+  30% {
+    transform: translate(0.3px, 0.2px) rotate(0deg);
+  }
+  40% {
+    transform: translate(0.1px, -0.1px) rotate(1deg);
+  }
+  50% {
+    transform: translate(-0.1px, 0.2px) rotate(-1deg);
+  }
+  60% {
+    transform: translate(-0.3px, 0.1px) rotate(0deg); 
+  }
+  70% {
+    transform: translate(0.3px, 0.1px) rotate(-1deg);
+  }
+  80% {
+    transform: translate(-0.1px, -0.1px) rotate(1deg);
+  }
+  90% {
+    transform: translate(0.1px, 0.2px) rotate(0deg);
+  }
+  100% {
+    transform: translate(0.1px, -0.2px) rotate(-1deg); 
+  }
+`;
+
 const questionStyle = css`
+  animation: ${trembleAnimation} 0.5s infinite;
   cursor: default;
   font-family: ${theme.fonts.secondary};
   font-size: ${theme.spacing.xxl}px;
