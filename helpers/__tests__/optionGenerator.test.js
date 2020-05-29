ce7758facf38efabc86c3f1daac94c7e33ded3d7
Ollie Crook <ollie@hubblehq.com>
2020-05-29 20:50:34 +0100

    Complete tests for case generator functions

--- helpers/__tests__/optionGenerator.test.js
+++ helpers/__tests__/optionGenerator.test.js
@@ -7,7 +7,7 @@ import {
   stringToMacroCase,
   stringToPascalCase,
   stringToSnakeCase,
-  stringToUppercase
+  stringToUppercase,
 } from '../optionGenerator';
 
 describe('optionGenerator : ', () => {
@@ -19,8 +19,36 @@ describe('optionGenerator : ', () => {
     });
 
     it('should convert a string to camelCase and set isCorrect to false when string is not "camel case"', () => {
-      const result = stringToCamelCase('kebab case');
-      const expected = { case: 'kebabCase', isCorrect: false };
+      const result = stringToCamelCase('nota case');
+      const expected = { case: 'notaCase', isCorrect: false };
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('stringToCobolCase : ', () => {
+    it('should convert a string to COBOL-CASE and set isCorrect to true when string is "cobol case"', () => {
+      const result = stringToCobolCase('cobol case');
+      const expected = { case: 'COBOL-CASE', isCorrect: true };
+      expect(result).toEqual(expected);
+    });
+
+    it('should convert a string to COBOL-CASE and set isCorrect to false when string is not "cobol case"', () => {
+      const result = stringToCobolCase('nota case');
+      const expected = { case: 'NOTA-CASE', isCorrect: false };
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('stringToFlatcase : ', () => {
+    it('should convert a string to flatcase and set isCorrect to true when string is "flat case"', () => {
+      const result = stringToFlatcase('flat case');
+      const expected = { case: 'flatcase', isCorrect: true };
+      expect(result).toEqual(expected);
+    });
+
+    it('should convert a string to flatcase and set isCorrect to false when string is not "flat case"', () => {
+      const result = stringToFlatcase('nota case');
+      const expected = { case: 'notacase', isCorrect: false };
       expect(result).toEqual(expected);
     });
   });
@@ -33,8 +61,50 @@ describe('optionGenerator : ', () => {
     });
 
     it('should convert a string to kebab-case and set isCorrect to false when string is not "kebab case"', () => {
-      const result = stringToKebabCase('snake case');
-      const expected = { case: 'snake-case', isCorrect: false };
+      const result = stringToKebabCase('nota case');
+      const expected = { case: 'nota-case', isCorrect: false };
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('stringToLeetCase : ', () => {
+    it('should convert a string to leet case and set isCorrect to true when string is "leet case"', () => {
+      const result = stringToLeetCase('1337 case');
+      const expected = { case: '1337 (@$3', isCorrect: true };
+      expect(result).toEqual(expected);
+    });
+
+    it('should convert a string to leetcase and set isCorrect to false when string is not "leet case"', () => {
+      const result = stringToLeetCase('nota case');
+      const expected = { case: '[]07@ (@$3', isCorrect: false };
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('stringToMacroCase : ', () => {
+    it('should convert a string to MACRO_CASE and set isCorrect to true when string is "macro case"', () => {
+      const result = stringToMacroCase('macro case');
+      const expected = { case: 'MACRO_CASE', isCorrect: true };
+      expect(result).toEqual(expected);
+    });
+
+    it('should convert a string to MACRO_CASE and set isCorrect to false when string is not "macro case"', () => {
+      const result = stringToMacroCase('nota case');
+      const expected = { case: 'NOTA_CASE', isCorrect: false };
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('stringToPascalCase : ', () => {
+    it('should convert a string to PascalCase and set isCorrect to true when string is "pascal case"', () => {
+      const result = stringToPascalCase('pascal case');
+      const expected = { case: 'PascalCase', isCorrect: true };
+      expect(result).toEqual(expected);
+    });
+
+    it('should convert a string to PascalCase and set isCorrect to false when string is not "pascal case"', () => {
+      const result = stringToPascalCase('nota case');
+      const expected = { case: 'NotaCase', isCorrect: false };
       expect(result).toEqual(expected);
     });
   });
@@ -47,8 +117,22 @@ describe('optionGenerator : ', () => {
     });
 
     it('should convert a string to snake_case and set isCorrect to false when string is not "snake case"', () => {
-      const result = stringToSnakeCase('camel case');
-      const expected = { case: 'camel_case', isCorrect: false };
+      const result = stringToSnakeCase('nota case');
+      const expected = { case: 'nota_case', isCorrect: false };
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('stringToUppercase : ', () => {
+    it('should convert a string to UPPERCASE and set isCorrect to true when string is "upper case"', () => {
+      const result = stringToUppercase('upper case');
+      const expected = { case: 'UPPERCASE', isCorrect: true };
+      expect(result).toEqual(expected);
+    });
+
+    it('should convert a string to UPPERCASE and set isCorrect to false when string is not "upper case"', () => {
+      const result = stringToUppercase('nota case');
+      const expected = { case: 'NOTACASE', isCorrect: false };
       expect(result).toEqual(expected);
     });
   });
