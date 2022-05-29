a8bf9258b93817b49a4909d46cf245cd117abafc
Ollie Crook <ollie@hubblehq.com>
2022-05-29 20:31:31 +0100

    Convert class React page to stateless hooks

---
+++ components/domain/Home.js
@@ -0,0 +1,217 @@
+import React, {useEffect, useRef, useState} from 'react';
+
+import {
+  stringToCamelCase,
+  stringToCobolCase,
+  stringToFlatcase,
+  stringToKebabCase,
+  stringToLeetCase,
+  stringToMacroCase,
+  stringToPascalCase,
+  stringToSnakeCase,
+  stringToUppercase,
+} from '../../helpers/optionGenerator';
+import GameOver from '../comps/GameOver';
+import PlayBoard from '../comps/PlayBoard';
+import ScoreBoard from '../comps/ScoreBoard';
+
+const DEFAULT_GAME_ARGS = ['camel case', 'kebab case', 'snake case'];
+const DEFAULT_GAME_QUESTIONS = [
+  stringToCamelCase,
+  stringToKebabCase,
+  stringToSnakeCase,
+];
+const DEFAULT_TIME_PER_ROUND = 3; // seconds
+
+const Home = () => {
+  let highScores;
+
+  const isReadyToShowHighScores = typeof window !== 'undefined';
+
+  if (isReadyToShowHighScores) {
+    highScores = JSON.parse(localStorage.getItem('highScore')) ?? null;
+  }
+
+  const intervalId = useRef();
+
+  const [currentQuestion, setCurrentQuestion] = useState(() => null);
+  const [possibleArgs, setPossibleArgs] = useState(() => DEFAULT_GAME_ARGS);
+  const [gameStatus, setGameStatus] = useState(() => 'idle');
+  const [questions, setQuestions] = useState(() => DEFAULT_GAME_QUESTIONS);
+  const [score, setScore] = useState(() => null);
+  const [countdown, setCountdown] = useState(() => DEFAULT_TIME_PER_ROUND);
+
+  useEffect(() => {
+    if (countdown === 0) {
+      clearInterval(intervalId.current);
+      timeOutQuiz();
+    }
+  }, [countdown]);
+
+  useEffect(() => {
+    return () => console.log('clear') || clearInterval(intervalId.current);
+  }, []);
+
+  const resetGameStateToDefault = () => {
+    setCountdown(DEFAULT_TIME_PER_ROUND);
+    setPossibleArgs(DEFAULT_GAME_ARGS);
+    setQuestions(DEFAULT_GAME_QUESTIONS);
+    setScore(0);
+  };
+
+  /**
+   * Uses helper functions to create questions to be displayed to the user.
+   * Also adds cases to the questions depending on score
+   * and restarts the timer for each question.
+   *
+   * @memberof Index
+   */
+  const createQuestion = () => {
+    /**
+     * Don't try to clear a timer that doesn't exist when instantiating a game
+     */
+    if (intervalId.current) {
+      clearInterval(intervalId.current);
+    }
+
+    const questionNum = Math.floor(Math.random() * questions.length);
+    const possibleArgsNum = Math.floor(Math.random() * possibleArgs.length);
+
+    setCurrentQuestion(questions[questionNum](possibleArgs[possibleArgsNum]));
+    setCountdown(DEFAULT_TIME_PER_ROUND);
+    startTimer();
+  };
+
+  /**
+   * Begins the 3 second countdown for each question.
+   *
+   * @memberof Index
+   */
+  const startTimer = () => {
+    intervalId.current = setInterval(() => {
+      setCountdown((prevState) => prevState - 1);
+    }, 1000);
+  };
+
+  /**
+   * Resets the scoreboard and starts the game.
+   * Triggers the creation of questions.
+   *
+   * @param {*} event Prevents default refresh behaviour of button submit events
+   * @memberof Index
+   */
+  const startGame = (event) => {
+    event.preventDefault();
+
+    resetGameStateToDefault();
+    setGameStatus('active');
+
+    createQuestion();
+  };
+
+  /**
+   * If the user doesn't click, this function is used to either select a new
+   * question or set the game to be over if they should have selected the answer.
+   *
+   * @memberof Index
+   */
+  const timeOutQuiz = () => {
+    if (!currentQuestion.isCorrect) {
+      createQuestion();
+    } else {
+      resetGameStateToDefault();
+      setGameStatus('game-over');
+    }
+  };
+
+  /**
+   * Triggered when a user clicks an answer.
+   * If the user is correct, increment score and repeat.
+   * If the user is incorrect, set game over.
+   *
+   * @param {*} isCorrect Bool used to determine if the answer is correct
+   * @memberof Index
+   */
+  const validateAnswer = (isCorrect) => {
+    clearInterval(intervalId.current);
+    if (isCorrect) {
+      const newScore = score + 1;
+      if (newScore >= 10 && !possibleArgs.includes('pascal case')) {
+        setPossibleArgs((prevValue) => [...prevValue, 'pascal case']);
+        setQuestions((prevValue) => [...prevValue, stringToPascalCase]);
+        setScore(newScore);
+      } else if (newScore >= 20 && !possibleArgs.includes('flat case')) {
+        setPossibleArgs((prevValue) => [...prevValue, 'flat case']);
+        setQuestions((prevValue) => [...prevValue, stringToFlatcase]);
+        setScore(newScore);
+      } else if (newScore >= 30 && !possibleArgs.includes('upper case')) {
+        setPossibleArgs((prevValue) => [...prevValue, 'upper case']);
+        setQuestions((prevValue) => [...prevValue, stringToUppercase]);
+        setScore(newScore);
+      } else if (newScore >= 40 && !possibleArgs.includes('cobol case')) {
+        setPossibleArgs((prevValue) => [...prevValue, 'cobol case']);
+        setQuestions((prevValue) => [...prevValue, stringToCobolCase]);
+        setScore(newScore);
+      } else if (newScore >= 50 && !possibleArgs.includes('macro case')) {
+        setPossibleArgs((prevValue) => [...prevValue, 'macro case']);
+        setQuestions((prevValue) => [...prevValue, stringToMacroCase]);
+        setScore(newScore);
+      } else if (newScore >= 60 && !possibleArgs.includes('1337 case')) {
+        setPossibleArgs((prevValue) => [...prevValue, '1337 case']);
+        setQuestions((prevValue) => [...prevValue, stringToLeetCase]);
+        setScore(newScore);
+      } else {
+        setScore(newScore);
+      }
+      createQuestion();
+    } else {
+      // Store scores locally for scoreboard
+      let newHighScores;
+      if (highScores) {
+        newHighScores = [...highScores, score]
+          .sort((a, b) => b - a)
+          .slice(0, 5);
+      } else {
+        newHighScores = [score];
+      }
+
+      localStorage.setItem('highScore', JSON.stringify(newHighScores));
+
+      setGameStatus('game-over');
+    }
+  };
+
+  if (gameStatus === 'idle') {
+    return (
+      <>
+        <ScoreBoard scores={highScores} />
+        <button type="button" onClick={startGame}>
+          Let’s Play
+        </button>
+      </>
+    );
+  } else if (gameStatus === 'active') {
+    return (
+      <PlayBoard
+        question={currentQuestion}
+        score={score}
+        timer={countdown}
+        validateAnswer={validateAnswer}
+      />
+    );
+  } else if (gameStatus === 'game-over') {
+    return (
+      <>
+        <GameOver score={score} />
+        <ScoreBoard scores={highScores} />
+        <button type="button" onClick={startGame}>
+          Let’s Play
+        </button>
+      </>
+    );
+  } else {
+    throw new Error('Could not get status of playing board.');
+  }
+};
+
+export default Home;
--- pages/index.js
+++ pages/index.js
@@ -1,254 +1,48 @@
+import {css, injectGlobal} from 'emotion';
+import dynamic from 'next/dynamic';
+import Head from 'next/head';
 import React from 'react';
 
-import GameOver from '../components/comps/GameOver';
-import PlayBoard from '../components/comps/PlayBoard';
-import ScoreBoard from '../components/comps/ScoreBoard';
+import Footer from '../components/comps/Footer';
 import Title from '../components/comps/Title';
-import PageLayout from '../components/layouts/PageLayout';
-import {
-  stringToCamelCase,
-  stringToCobolCase,
-  stringToFlatcase,
-  stringToKebabCase,
-  stringToLeetCase,
-  stringToMacroCase,
-  stringToPascalCase,
-  stringToSnakeCase,
-  stringToUppercase,
-} from '../helpers/optionGenerator';
 
-const homeMeta = {
+const HomeWithNoSSR = dynamic(() => import('../components/domain/Home'), {
+  ssr: false,
+});
+
+injectGlobal`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const pageLayoutStyle = css`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+const meta = {
   title: 'just-in_case',
   description: 'Can you be the case master?',
 };
 
-class Index extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.countDown = this.countDown.bind(this);
-    this.createQuestion = this.createQuestion.bind(this);
-    this.startGame = this.startGame.bind(this);
-    this.startTimer = this.startTimer.bind(this);
-    this.timeOutQuiz = this.timeOutQuiz.bind(this);
-
-    this.timer = null;
-
-    this.state = {
-      possibleArgs: ['camel case', 'kebab case', 'snake case'],
-      isGameOver: false,
-      hasStarted: false,
-      questions: [stringToCamelCase, stringToKebabCase, stringToSnakeCase],
-      score: 0,
-      timeLeft: 3,
-    };
-  }
-
-  componentDidMount() {
-    const highScores = JSON.parse(localStorage.getItem('highScore'));
-
-    if (highScores) {
-      this.setState({
-        scores: highScores,
-      });
-    }
-  }
-
-  /**
-   * Resets the scoreboard and starts the game.
-   * Triggers the creation of questions.
-   *
-   * @param {*} event Prevents default refresh behaviour of button submit events
-   * @memberof Index
-   */
-  startGame(event) {
-    event.preventDefault();
-
-    this.setState({
-      hasStarted: true,
-      isGameOver: false,
-      possibleArgs: ['camel case', 'kebab case', 'snake case'],
-      questions: [stringToCamelCase, stringToKebabCase, stringToSnakeCase],
-      score: 0,
-    });
-    this.createQuestion();
-  }
-
-  /**
-   * Uses helper functions to create questions to be displayed to the user.
-   * Also adds cases to the questions depending on score
-   * and restarts the timer for each question.
-   *
-   * @memberof Index
-   */
-  createQuestion() {
-    const { possibleArgs, questions } = this.state;
-    clearInterval(this.timer);
-
-    const questionNum = Math.floor(Math.random() * questions.length);
-    const possibleArgsNum = Math.floor(Math.random() * possibleArgs.length);
-
-    this.setState({
-      question: questions[questionNum](possibleArgs[possibleArgsNum]),
-      timeLeft: 3,
-    });
-
-    this.startTimer();
-  }
-
-  /**
-   * Begins the 3 second countdown for each question.
-   *
-   * @memberof Index
-   */
-  startTimer() {
-    this.timer = setInterval(() => {
-      this.countDown();
-      if (this.state.timeLeft === 0) {
-        clearInterval(this.timer);
-        this.timeOutQuiz();
-      }
-    }, 1000);
-  }
-
-  /**
-   * For each second that counts down, take 1 away from the clock.
-   *
-   * @memberof Index
-   */
-  countDown() {
-    this.setState({
-      timeLeft: this.state.timeLeft - 1,
-    });
-  }
-
-  /**
-   * If the user doesn't click, this function is used to either select a new
-   * question or set the game to be over if they should have selected the answer.
-   *
-   * @memberof Index
-   */
-  timeOutQuiz() {
-    const { question } = this.state;
-    if (!question.isCorrect) {
-      this.createQuestion();
-    } else {
-      this.setState({
-        hasStarted: false,
-        isGameOver: true,
-        score: 0,
-        startTimer: false,
-        timeLeft: 3,
-      });
-    }
-  }
-
-  /**
-   * Triggered when a user clicks an answer.
-   * If the user is correct, increment score and repeat.
-   * If the user is incorrect, set game over.
-   *
-   * @param {*} isCorrect Bool used to determine if the answer is correct
-   * @memberof Index
-   */
-  validateAnswer(isCorrect) {
-    const { possibleArgs, questions, score } = this.state;
-    clearInterval(this.timer);
-    if (isCorrect) {
-      const newScore = score + 1;
-      if (newScore >= 10 && possibleArgs.indexOf('pascal case') === -1) {
-        this.setState({
-          possibleArgs: [...possibleArgs, 'pascal case'],
-          questions: [...questions, stringToPascalCase],
-          score: newScore,
-        });
-      } else if (newScore >= 20 && possibleArgs.includes('flat case') === -1) {
-        this.setState({
-          possibleArgs: [...possibleArgs, 'flat case'],
-          questions: [...questions, stringToFlatcase],
-          score: newScore,
-        });
-      } else if (newScore >= 30 && possibleArgs.includes('upper case') === -1) {
-        this.setState({
-          possibleArgs: [...possibleArgs, 'upper case'],
-          questions: [...questions, stringToUppercase],
-          score: newScore,
-        });
-      } else if (newScore >= 40 && possibleArgs.includes('cobol case') === -1) {
-        this.setState({
-          possibleArgs: [...possibleArgs, 'cobol case'],
-          questions: [...questions, stringToCobolCase],
-          score: newScore,
-        });
-      } else if (newScore >= 50 && possibleArgs.includes('macro case') === -1) {
-        this.setState({
-          possibleArgs: [...possibleArgs, 'macro case'],
-          questions: [...questions, stringToMacroCase],
-          score: newScore,
-        });
-      } else if (newScore >= 60 && possibleArgs.includes('1337 case') === -1) {
-        this.setState({
-          possibleArgs: [...possibleArgs, '1337 case'],
-          questions: [...questions, stringToLeetCase],
-          score: newScore,
-        });
-      } else {
-        this.setState({
-          score: newScore,
-        });
-      }
-
-      this.createQuestion();
-    } else {
-      // Store scores locally for scoreboard
-      const oldHighScores = JSON.parse(localStorage.getItem('highScore'));
-      let newHighScores;
-      if (oldHighScores) {
-        newHighScores = [...oldHighScores, score]
-          .sort((a, b) => b - a)
-          .slice(0, 5);
-      } else {
-        newHighScores = [score];
-      }
-
-      localStorage.setItem('highScore', JSON.stringify(newHighScores));
-
-      this.setState({
-        hasStarted: false,
-        isGameOver: true,
-        scores: newHighScores,
-      });
-    }
-  }
-
-  render() {
-    const {
-      hasStarted,
-      isGameOver,
-      question,
-      score,
-      scores,
-      timeLeft,
-    } = this.state;
-
-    return (
-      <PageLayout meta={homeMeta}>
-        <Title />
-        {!hasStarted && scores ? <ScoreBoard scores={scores} /> : null}
-        {hasStarted ? (
-          <PlayBoard
-            question={question}
-            score={score}
-            timer={timeLeft}
-            validateAnswer={this.validateAnswer.bind(this)}
-          />
-        ) : (
-          <input type="submit" onClick={this.startGame} value="Let's Play" />
-        )}
-        {isGameOver && <GameOver score={score} />}
-      </PageLayout>
-    );
-  }
-}
+const Index = () => (
+  <>
+    <Head>
+      <title>{meta.title}</title>
+      <link rel="shortcut icon" href="/static/favicon.ico" />
+      <meta name="description" content={meta.description} />
+    </Head>
+    <div className={pageLayoutStyle}>
+      <Title />
+      <HomeWithNoSSR />
+      <Footer />
+    </div>
+  </>
+);
 
 export default Index;
